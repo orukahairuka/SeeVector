3bfb4b33a1897fff9167b33a3f16d4aaa2faa626
Erika Sakurai <erika03301109@gmail.com>
2025-11-28 12:48:10 +0900

    Initial Commit

---
+++ SeeVector/SeeVectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeVectorApp.swift
+//  SeeVector
+//
+//  Created by Sakurai Erika on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SeeVectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeeVectorTests/SeeVectorTests.swift
@@ -0,0 +1,16 @@
+//
+//  SeeVectorTests.swift
+//  SeeVectorTests
+//
+//  Created by Sakurai Erika on 2025/11/28.
+//
+
+import Testing
+
+struct SeeVectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
